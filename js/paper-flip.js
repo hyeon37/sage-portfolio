1c13873cd9b3bdae6e048cd6b56eb849e322e428
hyeon37 <hyeon37.2525@gmail.com>
2026-01-11 02:58:04 +0900

    paper-flip 수정중

--- js/paper-flip.js
+++ js/paper-flip.js
@@ -1,18 +1,84 @@
 // 초기 화면 설정
 const initPaperFlip = () => {
-  var el = document.querySelector(".paper.curr .back");
-  el.addEventListener("click", function (e) {
-    console.log("e", e, this);
-    // this.classList.add("flip");
-    // this.previousElementSibling.classList.add("flip");
-    document.documentElement.style.setProperty(
-      "--paper-flip-width",
-      "calc(100vw - 20vw)"
-    );
-    document.documentElement.style.setProperty(
-      "--paper-flip-height",
-      "calc(100vh - 10vh)"
-    );
+  let backs = document.querySelectorAll(".back");
+  backs.forEach((back) => {
+    back.addEventListener("click", (event) => {
+      console.log("back", event.target);
+      if (event.target.parentElement.classList.contains("curr")) {
+        document.documentElement.style.setProperty(
+          "--paper-flip-width-curr",
+          "calc(100vw - 20vw)"
+        );
+        document.documentElement.style.setProperty(
+          "--paper-flip-height-curr",
+          "calc(100vh - 10vh)"
+        );
+        console.log("e", event, event.target);
+        const t = setTimeout(() => {
+          event.target.classList.add("flip");
+          event.target.previousElementSibling.classList.add("flip");
+          event.target.parentElement.classList.add("prev");
+          event.target.parentElement.classList.remove("curr");
+          event.target.parentElement.nextElementSibling.classList.add("curr");
+          document.documentElement.style.setProperty(
+            "--paper-flip-width-prev",
+            "calc(100vw - 20vw)"
+          );
+          document.documentElement.style.setProperty(
+            "--paper-flip-height-prev",
+            "calc(100vh - 10vh)"
+          );
+          document.documentElement.style.setProperty(
+            "--paper-flip-width-curr",
+            "5rem"
+          );
+          document.documentElement.style.setProperty(
+            "--paper-flip-height-curr",
+            "5rem"
+          );
+          event.target.parentElement.previousElementSibling?.classList.remove("prev");
+          Array.from(event.target.parentElement.previousElementSibling?.children || []).forEach((child) => {
+            child.classList.remove("flip");
+          });
+          clearTimeout(t);
+        }, 1000);
+      } else if (event.target.parentElement.classList.contains("prev")) {
+        document.documentElement.style.setProperty(
+          "--paper-flip-width-prev",
+          "5rem"
+        );
+        document.documentElement.style.setProperty(
+          "--paper-flip-height-prev",
+          "5rem"
+        );
+        console.log("e", event, event.target);
+        event.target.classList.remove("flip");
+        event.target.previousElementSibling.classList.remove("flip");
+        event.target.parentElement.classList.remove("prev");
+        event.target.parentElement.classList.add("curr");
+        event.target.parentElement.nextElementSibling.classList.remove("curr");
+        document.documentElement.style.setProperty(
+          "--paper-flip-width-curr",
+          "5rem"
+        );
+        document.documentElement.style.setProperty(
+          "--paper-flip-height-curr",
+          "5rem"
+        );
+        document.documentElement.style.setProperty(
+          "--paper-flip-width-prev",
+          "calc(100vw - 20vw)"
+        );
+        document.documentElement.style.setProperty(
+          "--paper-flip-height-prev",
+          "calc(100vh - 10vh)"
+        );
+        event.target.parentElement.previousElementSibling?.classList.add("prev");
+        Array.from(event.target.parentElement.previousElementSibling?.children || []).forEach((child) => {
+          child.classList.add("flip");
+        });
+      }
+    });
   });
 };
 
