b95f759bec212daec55eb5a0d3de9b032ddeb88f
hyeon <hyeon37.2525@gmail.com>
2026-01-12 13:46:33 +0900

    update paper-flip

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 README
 
-hyeon37 untitled page
+Sage Portfolio
+
+static web page
+
+Github actions + Docker hub + GCE
+Github actions ci/cd
+Docker hub image build
+GCE docker-compose Docker image run
+Google Workload Identify auth
--- js/paper-flip.js
+++ js/paper-flip.js
@@ -7,11 +7,11 @@ const initPaperFlip = () => {
       if (event.target.parentElement.classList.contains("curr")) {
         document.documentElement.style.setProperty(
           "--paper-flip-width-curr",
-          "calc(100vw - 20vw)"
+          "calc(100% - 20vw)"
         );
         document.documentElement.style.setProperty(
           "--paper-flip-height-curr",
-          "calc(100vh - 10vh)"
+          "calc(100% - 10vh)"
         );
         console.log("e", event, event.target);
         const t = setTimeout(() => {
@@ -22,19 +22,19 @@ const initPaperFlip = () => {
           event.target.parentElement.nextElementSibling.classList.add("curr");
           document.documentElement.style.setProperty(
             "--paper-flip-width-prev",
-            "calc(100vw - 20vw)"
+            "calc(100% - 20vw)"
           );
           document.documentElement.style.setProperty(
             "--paper-flip-height-prev",
-            "calc(100vh - 10vh)"
+            "calc(100% - 10vh)"
           );
           document.documentElement.style.setProperty(
             "--paper-flip-width-curr",
-            "5rem"
+            "10rem"
           );
           document.documentElement.style.setProperty(
             "--paper-flip-height-curr",
-            "5rem"
+            "10rem"
           );
           event.target.parentElement.previousElementSibling?.classList.remove("prev");
           Array.from(event.target.parentElement.previousElementSibling?.children || []).forEach((child) => {
@@ -45,11 +45,11 @@ const initPaperFlip = () => {
       } else if (event.target.parentElement.classList.contains("prev")) {
         document.documentElement.style.setProperty(
           "--paper-flip-width-prev",
-          "5rem"
+          "10rem"
         );
         document.documentElement.style.setProperty(
           "--paper-flip-height-prev",
-          "5rem"
+          "10rem"
         );
         console.log("e", event, event.target);
         event.target.classList.remove("flip");
@@ -59,19 +59,19 @@ const initPaperFlip = () => {
         event.target.parentElement.nextElementSibling.classList.remove("curr");
         document.documentElement.style.setProperty(
           "--paper-flip-width-curr",
-          "5rem"
+          "10rem"
         );
         document.documentElement.style.setProperty(
           "--paper-flip-height-curr",
-          "5rem"
+          "10rem"
         );
         document.documentElement.style.setProperty(
           "--paper-flip-width-prev",
-          "calc(100vw - 20vw)"
+          "calc(100% - 20vw)"
         );
         document.documentElement.style.setProperty(
           "--paper-flip-height-prev",
-          "calc(100vh - 10vh)"
+          "calc(100% - 10vh)"
         );
         event.target.parentElement.previousElementSibling?.classList.add("prev");
         Array.from(event.target.parentElement.previousElementSibling?.children || []).forEach((child) => {
