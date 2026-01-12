5d7fa658e3bc22b838b4d760656826c5a44533ff
hyeon <hyeon37.2525@gmail.com>
2026-01-12 13:54:15 +0900

    update paper-flip

--- js/paper-flip.js
+++ js/paper-flip.js
@@ -7,11 +7,11 @@ const initPaperFlip = () => {
       if (event.target.parentElement.classList.contains("curr")) {
         document.documentElement.style.setProperty(
           "--paper-flip-width-curr",
-          "calc(100% - 20vw)"
+          "calc(100vw - 20vw)"
         );
         document.documentElement.style.setProperty(
           "--paper-flip-height-curr",
-          "calc(100% - 10vh)"
+          "calc(100vh - 10vh)"
         );
         console.log("e", event, event.target);
         const t = setTimeout(() => {
@@ -22,11 +22,11 @@ const initPaperFlip = () => {
           event.target.parentElement.nextElementSibling.classList.add("curr");
           document.documentElement.style.setProperty(
             "--paper-flip-width-prev",
-            "calc(100% - 20vw)"
+            "calc(100vw - 20vw)"
           );
           document.documentElement.style.setProperty(
             "--paper-flip-height-prev",
-            "calc(100% - 10vh)"
+            "calc(100vh - 10vh)"
           );
           document.documentElement.style.setProperty(
             "--paper-flip-width-curr",
@@ -67,11 +67,11 @@ const initPaperFlip = () => {
         );
         document.documentElement.style.setProperty(
           "--paper-flip-width-prev",
-          "calc(100% - 20vw)"
+          "calc(100vw - 20vw)"
         );
         document.documentElement.style.setProperty(
           "--paper-flip-height-prev",
-          "calc(100% - 10vh)"
+          "calc(100vh - 10vh)"
         );
         event.target.parentElement.previousElementSibling?.classList.add("prev");
         Array.from(event.target.parentElement.previousElementSibling?.children || []).forEach((child) => {
